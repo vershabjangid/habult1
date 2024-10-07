d9fe9bb3713b9b27faae2872a9c68e0d38a30bd3
vershab jangid <vershabjangid@yahoo.com>
2024-10-07 23:41:30 +0530

    front

--- amdmin-panel/src/pages/webpages/chartdata.js
+++
@@ -1,5 +0,0 @@
-
-export const dataset = [
-  { gb: 0 },
-  { gb: 25 },
-];
--- server/app/routes/admin/contact.js
+++
@@ -1,7 +0,0 @@
-let express = require('express')
-const { contactus } = require('../../controller/admin/Contact_us')
-let contactroute = express.Router()
-
-contactroute.post('/contact',contactus)
-
-module.exports = contactroute;
