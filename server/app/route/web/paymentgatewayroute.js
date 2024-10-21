97be340f91b441a86e31c2a6a65b1ff1016b5356
vershab jangid <vershabjangid@yahoo.com>
2024-10-21 14:10:32 +0530

    newpagw

--- server/app/route/web/paymentgatewayroute.js
+++ server/app/route/web/paymentgatewayroute.js
@@ -2,7 +2,7 @@ let express = require('express');
 const { payementgatewaycontroller } = require('../../controller/web/PaymentGateway');
 let paymentgateway = express.Router()
 
-paymentgateway.get('/payment', payementgatewaycontroller)
+// paymentgateway.get('/payment', payementgatewaycontroller)
 
 // paymentgateway.post('/verify-payment', paymentgateway)
 
