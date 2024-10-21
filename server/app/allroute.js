17adfd986ac8063ce5afc0b14898ac2b3ea89b54
vershab jangid <vershabjangid@yahoo.com>
2024-10-21 15:23:14 +0530

    jai shree ram

--- server/app/allroute.js
+++ server/app/allroute.js
@@ -1,11 +1,9 @@
 let express = require('express');
 const webauthroutes = require('./route/web/webauthroutes');
-const paymentgateway = require('./route/web/paymentgatewayroute');
 let allroutes = express.Router();
 
 
 allroutes.use(webauthroutes);
-allroutes.use(paymentgateway)
 
 
 module.exports = allroutes;
