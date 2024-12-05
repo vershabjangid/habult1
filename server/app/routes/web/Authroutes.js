f7cb27d25a91e2b663e9e1fd185b4f82099d6407
vershab jangid <vershabjangid@yahoo.com>
2024-12-06 02:08:27 +0530

    hello

--- server/app/routes/web/Authroutes.js
+++ server/app/routes/web/Authroutes.js
@@ -1,11 +1,15 @@
 let express = require('express');
-const { register, verifyotp, resendotp } = require('../../controller/web/Auth');
+const { register, verifyotp, resendotp, viewregister } = require('../../controller/web/Auth');
 let authroutes = express.Router()
 
 
 authroutes.post('/register', register);
 authroutes.post('/verify-register', verifyotp);
 authroutes.put('/resend-otp', resendotp);
+
+
+authroutes.get('/view-register', viewregister)
+
 
 
 
