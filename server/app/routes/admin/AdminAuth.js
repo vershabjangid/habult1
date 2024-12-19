132cc0c5368cf5c4050a2e8b1f123668d650920f
vershab jangid <vershabjangid@yahoo.com>
2024-12-19 21:46:25 +0530

    hello

--- server/app/routes/admin/AdminAuth.js
+++
@@ -1,7 +0,0 @@
-let express = require('express')
-const { dashlogin } = require('../../controller/admin/Login')
-let adminauth = express.Router()
-
-adminauth.post('/admin-login',dashlogin)
-
-module.exports = adminauth
